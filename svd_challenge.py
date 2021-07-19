fixed computation of condition number. encountered problem: ARPACK error

svd_challenge.py
@@ -68,25 +68,31 @@ class SVD(object):
             return
 
     def decomposition_singular_values(self, A):
-        U, s, Vh = sparse.svds(A, k=1)
-        return s
+        U, s, Vh = sparse.svds(A, k=1, which='LM')
+        U, s2, Vh = sparse.svds(A, k=1, which='SM')
+        print(s, s2)
+        return s, s2
 
-    def cond_num(self, s):
+    def cond_num(self, sin, sin2):
         # Computes the condition number
-        c = s[-1]/s[0]
+        c = sin/sin2
         return c
 
 
-A = SVD().random_s_matrix(5, 5, 0.1, value_type="binary")
+A = SVD().random_s_matrix(500, 500, 0.1, value_type="binary")
 ''' 13 x 7 seem to be the standard minimum dimensions required for the use of scipy.linalg.sparse.svds().
     This changes relative to what k (number of singular values/vectors to be computed) is defined as during the 
-    usage of scipy...svds(). '''
+    usage of scipy...svds(). If k is not defined by the user, it is set as k=6. '''
 
 #svd_timer.start()
-SVD().cond_num(SVD().decomposition_singular_values(A))
+#SVD().cond_num(SVD().decomposition_singular_values(A))
 #svd_timer.stop()
+try:
+    s, s2 = SVD().decomposition_singular_values(A)
+    print(SVD().cond_num(s, s2))
+except ValueError:
+    pass
 
-# print(SVD().cond_num(SVD().decomposition_singular_values(A)))
 # print(random_s_matrix(10, 10, 0.25, "binary", r_seed=15).toarray())
 # plt.imshow(A.toarray())
 # plt.show()